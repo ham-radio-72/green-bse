84816fa25a39b0beed0e56e08cfd0470ccb13cb7
Ming Wen <wenming2026@gmail.com>
2026-01-13 18:21:26 -0500

    modified README for N2_STO3G

--- example/N2_STO3G/bse_singlet.sh
+++ example/N2_STO3G/bse_singlet.sh
@@ -1,14 +1,13 @@
 #!/bin/bash 
 
 export SCRIPTDIR=../../script
-export SIMDIR=./scgw
 export IRDIR=./irgrid
 
 python -u $SCRIPTDIR/solveCasida_main.py \
        --type singlet --beta 1000 \
        --qpac 1 --iter -1 --iter_W -1 \
        --calc_pi 1 --monitor 1 \
-       --sim $SIMDIR/sim.h5 \
+       --sim ./sim.h5 \
        --int_path ./df_hf_int/ \
        --ir_file $IRDIR/1e5.h5 \
        --input ./input.h5 \
