d275cc0af62b644c4fa720b5874b383bcf7d9226
Ming Wen <wenming2026@gmail.com>
2026-01-13 18:13:57 -0500

    remove N2_ccpvdz to add N2_STO3G to reduce size

--- example/N2_STO3G/check_MO_molden.sh
+++ example/N2_STO3G/check_MO_molden.sh
@@ -6,6 +6,6 @@ python -u $SCRIPTDIR/ph_excitation_MO.py \
        --bse_file ./bse_singlet.h5 \
        --coords_file ./nitrogen_coords.dat \
        --molden_path molden \
-       --basis ccpvdz \
+       --basis sto-3g \
        --excitation_num 5
        
